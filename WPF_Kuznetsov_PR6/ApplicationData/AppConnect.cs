a5fecae235b55c0fb504a80798ca09b09fb7bb58
Алексей Писарев <afonasiu2983@gmail.com>
2022-12-14 13:56:40 +1000

    Добавьте файлы проекта.

---
+++ WPF_Kuznetsov_PR6/ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Kuznetsov_PR6.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static PR6Entities mode10db;
+    }
+}
---
+++ WPF_Kuznetsov_PR6/PageMain/PageLogin.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPF_Kuznetsov_PR6.ApplicationData;
+
+namespace WPF_Kuznetsov_PR6.PageMain
+{
+    /// <summary>
+    /// Логика взаимодействия для PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var userObj = AppConnect.mode10db.User.FirstOrDefault(x => x.login == txbLogin.Text && x.password == psbPassword.Password);
+                Window1 window1 = new Window1();
+                if (userObj == null)
+                {
+                    MessageBox.Show("Такого пользователя нет!", "Ошибка авторизации!", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+                else
+                {
+                    switch (userObj.idRole)
+                    {
+                        case 1:
+                            MessageBox.Show("Здравствуйте, Администратор " + userObj.name + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            window1.Show();
+                            break;
+                        case 2:
+                            MessageBox.Show("Здравствуйте, Ученик " + userObj.name + "!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                            window1.Show();
+                            break;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show("Ошибка" + Ex.Message.ToString() + "Критическая ошибка приложения!", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+    }
+}
